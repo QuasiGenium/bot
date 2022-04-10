c438a9a6ca9a0b4b3f7ed4622207c66196e78714
QuasiGenium <Anotherzerosun@gmail.com>
2022-04-10 23:03:48 +0300

    Tri commit

--- hab/make_pic.py
+++ hab/make_pic.py
@@ -1,10 +1,17 @@
 from PIL import Image, ImageDraw, ImageFilter
 
+'''
 board1 = Image.open('pic/board.png')
 w = Image.open('pic/w_pes.png')
 mask_im = Image.open('pic/w_pes.png').resize(w.size).convert('L')
 board = board1.copy()
 board.paste(w, (23 + 60, 2), mask_im)
 board.save('pic/board1.png', quality=95)
+'''
+
+
 def make_picture(f):
-    pass
+    board1 = Image.open('pic/board.png')
+    for i in f:
+        for j in i:
+            pass
